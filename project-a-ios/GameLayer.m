12116eb11f4e4ee3c15ab3420d9dc1c1e496f4fc
Mike <ezuratechan@gmail.com>
2013-02-08 13:05:25 -0600

    drawing pixels!!!

--- project-a-ios/GameLayer.m
+++ project-a-ios/GameLayer.m
@@ -39,6 +39,61 @@
         [ self appendNewGround ];
         [ self appendNewGround ];
         [ self appendNewGround ];
+        
+        
+        
+        CCSprite *sprite=[grounds objectAtIndex:0];
+        
+        CGPoint local=ccp( 0, 0 );
+
+        CGPoint activeLocal1=ccp( 0, 0 );
+        CGPoint activeLocal2=ccp( 1, 0 );
+        CGPoint activeLocal3=ccp( 2, 0 );
+        CGPoint activeLocal4=ccp( 3, 0 );
+        
+        CGPoint activeLocal5=ccp( 4, 0 );
+        CGPoint activeLocal6=ccp( 5, 0 );
+        CGPoint activeLocal7=ccp( 6, 0 );
+        CGPoint activeLocal8=ccp( 7, 0 );
+        
+        CGPoint activeLocal9=ccp( 8,  0);
+        CGPoint activeLocal10=ccp( 9,  0);
+        CGPoint activeLocal11=ccp( 10, 0 );
+        CGPoint activeLocal12=ccp( 11, 0 );
+        
+        CGPoint activeLocal13=ccp( 12, 0 );
+        CGPoint activeLocal14=ccp( 13, 0 );
+        CGPoint activeLocal15=ccp( 14, 0 );
+        CGPoint activeLocal16=ccp( 15, 0 );
+        
+         
+        CCMutableTexture2D* groundMutableTexture=(CCMutableTexture2D*)(sprite.texture);
+        
+        //[groundMutableTexture drawLineFrom:activeLocal to:local withLineWidth:DRAW_WIDTH andColor:currentColor];
+        ccColor4B color = ccc4( 0, 0, 0, 255 );
+        
+        [groundMutableTexture setPixelAt: activeLocal1 rgba: color ];
+        [groundMutableTexture setPixelAt: activeLocal2 rgba: color ];
+        [groundMutableTexture setPixelAt: activeLocal3 rgba: color ];
+        [groundMutableTexture setPixelAt: activeLocal4 rgba: color ];
+        
+        [groundMutableTexture setPixelAt: activeLocal5 rgba: color ];
+        [groundMutableTexture setPixelAt: activeLocal6 rgba: color ];
+        [groundMutableTexture setPixelAt: activeLocal7 rgba: color ];
+        [groundMutableTexture setPixelAt: activeLocal8 rgba: color ];
+        [groundMutableTexture setPixelAt: activeLocal9 rgba: color ];
+        [groundMutableTexture setPixelAt: activeLocal10 rgba: color ];
+        [groundMutableTexture setPixelAt: activeLocal11 rgba: color ];
+        [groundMutableTexture setPixelAt: activeLocal12 rgba: color ];
+        [groundMutableTexture setPixelAt: activeLocal13 rgba: color ];
+        [groundMutableTexture setPixelAt: activeLocal14 rgba: color ];
+        [groundMutableTexture setPixelAt: activeLocal15 rgba: color ];
+        [groundMutableTexture setPixelAt: activeLocal16 rgba: color ];
+        
+        
+        [groundMutableTexture apply]; //Redraw texture
+
+        
 		
 	}
 	return self;
