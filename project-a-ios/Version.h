ebf9cb7a24bb88a6471c549cdfb97305c063ff6e
Mike <ezuratechan@gmail.com>
2013-05-07 23:00:13 -0500

    updated version to v0.04.3

--- project-a-ios/Version.h
+++ project-a-ios/Version.h
@@ -1,6 +1,6 @@
 // Version.h 
 // project-a-ios 
 // 
-// Created by Mike Bell on Tue May  7 01:14:44 2013
+// Created by Mike Bell on Tue May  7 22:59:25 2013
 
-#define GAME_VERSION @"v0.04.2"
+#define GAME_VERSION @"v0.04.3"
