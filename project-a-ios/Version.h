0292abd687da7d93c1e20cefd783ddaf85641f56
Mike <ezuratechan@gmail.com>
2013-06-10 12:14:18 -0500

    updated version

--- project-a-ios/Version.h
+++ project-a-ios/Version.h
@@ -1,6 +1,6 @@
 // Version.h 
 // project-a-ios 
 // 
-// Created by Mike Bell on Thu Jun  6 18:23:48 2013
+// Created by Mike Bell on Mon Jun 10 12:13:53 2013
 
-#define GAME_VERSION @"v0.1.1"
+#define GAME_VERSION @"v0.1.2"
