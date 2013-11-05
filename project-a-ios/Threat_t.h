49cb0d5936836e7402e32b899533eb2f411537a0
Mike <ezuratechan@gmail.com>
2013-11-05 09:28:51 -0600

    added GPL to each source file in dev

--- project-a-ios/Threat_t.h
+++ project-a-ios/Threat_t.h
@@ -1,3 +1,22 @@
+/*
+Project A
+A dungeon crawler by Mike Bell
+
+This file is part of Project A.
+ 
+Project A is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Project A is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied
+warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+PURPOSE.  See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+*/
 //  Threat_t.h
 //  project-a-ios
 //
